c8592ae22c66a7b6581fd3c1d675b95ca22cef32
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-10-31 09:49:52 +0800

    update docs link

--- frontend/src/lib/navigation.ts
+++ frontend/src/lib/navigation.ts
@@ -4,6 +4,6 @@ export const DEFI_URL = "/defi";
 export const SUILEND_URL = "https://suilend.fi";
 
 export const DOCS_URL =
-  "https://docs.suilend.fi/faq/intro-to-springsui-standard";
+  "https://docs.suilend.fi/springsui/intro-to-springsui-standard";
 export const TWITTER_URL = "https://twitter.com/springsui_";
 export const DISCORD_URL = "https://discord.com/invite/suilend";
