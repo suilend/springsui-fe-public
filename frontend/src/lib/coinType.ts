148fa8b4ef256831ce1dc6b128e1b916cd421f86
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-04 10:22:01 +0800

    add overrides

--- frontend/src/lib/coinType.ts
+++ frontend/src/lib/coinType.ts
@@ -1,9 +1,14 @@
 import { normalizeStructTag } from "@mysten/sui/utils";
 
 export const LIQUID_STAKING_INFO = {
-  id: "0x15eda7330c8f99c30e430b4d82fd7ab2af3ead4ae17046fcb224aa9bad394f6b",
-  type: "0x83556891f4a0f233ce7b05cfe7f957d4020492a34f5405b2cb9377d060bef4bf::spring_sui::SPRING_SUI",
+  id:
+    process.env.NEXT_PUBLIC_LIQUID_STAKING_INFO_ID ??
+    "0x15eda7330c8f99c30e430b4d82fd7ab2af3ead4ae17046fcb224aa9bad394f6b",
+  type:
+    process.env.NEXT_PUBLIC_LIQUID_STAKING_INFO_TYPE ??
+    "0x83556891f4a0f233ce7b05cfe7f957d4020492a34f5405b2cb9377d060bef4bf::spring_sui::SPRING_SUI",
   weightHookId:
+    process.env.NEXT_PUBLIC_LIQUID_STAKING_INFO_WEIGHT_HOOK_ID ??
     "0xbbafcb2d7399c0846f8185da3f273ad5b26b3b35993050affa44cfa890f1f144",
 };
 
