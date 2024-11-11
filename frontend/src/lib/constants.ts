01b6d2716d6f99520702f666eade54b381b18344
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-11 13:30:04 +0800

    fix apostrophe

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -4,4 +4,4 @@ export const TWITTER = "@springsui_";
 
 export const TITLE = "SpringSui | Infinitely liquid staking on Sui";
 export const DESCRIPTION =
-  "Stake SUI with SpringSui and access the power of DeFi with sSUI, our flagship LST. With SpringSui’s liquid staking, you can stake SUI and hold sSUI to earn yield while maintaining flexibility. Maximize your DeFi opportunities and enjoy rewards with sSUI, the LST that brings unmatched liquidity to Sui!";
+  "Stake SUI with SpringSui and access the power of DeFi with sSUI, our flagship LST. With SpringSui's liquid staking, you can stake SUI and hold sSUI to earn yield while maintaining flexibility. Maximize your DeFi opportunities and enjoy rewards with sSUI, the LST that brings unmatched liquidity to Sui!";
