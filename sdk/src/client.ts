90eb3c51fb52102e42b17cbb29a43a821268c1af
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-01-21 14:57:42 +0800

    refactor sdk

---
+++ sdk/src/client.ts
@@ -0,0 +1,406 @@
+import { SuiClient } from "@mysten/sui/client";
+import {
+  Transaction,
+  TransactionObjectInput,
+  coinWithBalance,
+} from "@mysten/sui/transactions";
+import { SUI_DECIMALS } from "@mysten/sui/utils";
+import BigNumber from "bignumber.js";
+
+import { phantom } from "./_generated/_framework/reified";
+import { PACKAGE_ID, setPublishedAt } from "./_generated/liquid_staking";
+import {
+  newBuilder,
+  setRedeemFeeBps,
+  setSpreadFeeBps,
+  setSuiMintFeeBps,
+  toFeeConfig,
+} from "./_generated/liquid_staking/fees/functions";
+import * as generated from "./_generated/liquid_staking/liquid-staking/functions";
+import { LiquidStakingInfo } from "./_generated/liquid_staking/liquid-staking/structs";
+import * as weightHookGenerated from "./_generated/liquid_staking/weight/functions";
+import { WeightHook } from "./_generated/liquid_staking/weight/structs";
+
+export interface LiquidStakingObjectInfo {
+  id: string;
+  type: string;
+  weightHookId: string;
+}
+
+export interface FeeConfigArgs {
+  mintFeeBps?: number;
+  redeemFeeBps?: number;
+  spreadFeeBps?: number;
+}
+
+const SUI_SYSTEM_STATE_ID =
+  "0x0000000000000000000000000000000000000000000000000000000000000005";
+export const SUILEND_VALIDATOR_ADDRESS =
+  "0xce8e537664ba5d1d5a6a857b17bd142097138706281882be6805e17065ecde89";
+const SPRING_SUI_UPGRADE_CAP_ID =
+  "0x4dc657b6c0fe896f4b94fee1ceac96312dde0a36b94e799caaec30deb53dcd67";
+
+async function getLatestPackageId(
+  client: SuiClient,
+  upgradeCapId: string,
+): Promise<string> {
+  const object = await client.getObject({
+    id: upgradeCapId,
+    options: {
+      showContent: true,
+    },
+  });
+
+  return (object.data?.content as unknown as any).fields.package;
+}
+
+export class LstClient {
+  liquidStakingObject: LiquidStakingObjectInfo;
+  client: SuiClient;
+
+  static async initialize(
+    client: SuiClient,
+    liquidStakingObjectInfo: LiquidStakingObjectInfo,
+  ): Promise<LstClient> {
+    const publishedAt = await getLatestPackageId(
+      client,
+      SPRING_SUI_UPGRADE_CAP_ID,
+    );
+    setPublishedAt(publishedAt);
+    console.log(`Initialized LstClient with package ID: ${publishedAt}`);
+
+    return new LstClient(liquidStakingObjectInfo, client);
+  }
+
+  static createNewLst(
+    tx: Transaction,
+    treasuryCap: string,
+    coinType: string,
+  ): TransactionObjectInput {
+    const [feeConfigBuilder] = newBuilder(tx);
+    const [feeConfig] = toFeeConfig(tx, feeConfigBuilder);
+
+    const [adminCap, liquidStakingInfo] = generated.createLst(tx, coinType, {
+      feeConfig,
+      lstTreasuryCap: treasuryCap,
+    });
+
+    tx.moveCall({
+      target: `0x2::transfer::public_share_object`,
+      typeArguments: [`${LiquidStakingInfo.$typeName}<${coinType}>`],
+      arguments: [liquidStakingInfo],
+    });
+
+    const [weightHook, weightHookAdminCap] = weightHookGenerated.new_(
+      tx,
+      coinType,
+      adminCap,
+    );
+
+    tx.moveCall({
+      target: `0x2::transfer::public_share_object`,
+      typeArguments: [`${WeightHook.$typeName}<${coinType}>`],
+      arguments: [weightHook],
+    });
+
+    return weightHookAdminCap;
+  }
+
+  static async getWeightHookAdminCapId(
+    client: SuiClient,
+    address: string,
+    weightHookAdminCapId: string,
+  ): Promise<string | null | undefined> {
+    const res = (
+      await client.getOwnedObjects({
+        owner: address,
+        filter: {
+          StructType: `${PACKAGE_ID}::weight::WeightHookAdminCap<${weightHookAdminCapId}>`,
+        },
+      })
+    ).data;
+
+    if (res.length == 0) {
+      return null;
+    }
+
+    return res[0].data?.objectId;
+  }
+
+  constructor(liquidStakingObject: LiquidStakingObjectInfo, client: SuiClient) {
+    this.liquidStakingObject = liquidStakingObject;
+    this.client = client;
+  }
+
+  async getAdminCapId(address: string): Promise<string | null | undefined> {
+    const res = (
+      await this.client.getOwnedObjects({
+        owner: address,
+        filter: {
+          StructType: `${PACKAGE_ID}::liquid_staking::AdminCap<${this.liquidStakingObject.type}>`,
+        },
+      })
+    ).data;
+
+    if (res.length == 0) {
+      return null;
+    }
+
+    return res[0].data?.objectId;
+  }
+
+  // returns the lst object
+  mint(tx: Transaction, suiCoinId: TransactionObjectInput) {
+    const [lst] = generated.mint(tx, this.liquidStakingObject.type, {
+      self: this.liquidStakingObject.id,
+      sui: suiCoinId,
+      systemState: SUI_SYSTEM_STATE_ID,
+    });
+
+    return lst;
+  }
+  mintAmountAndRebalance(tx: Transaction, address: string, amount: string) {
+    const [sui] = tx.splitCoins(tx.gas, [BigInt(amount)]);
+    const lst = this.mint(tx, sui);
+
+    this.rebalance(tx, this.liquidStakingObject.weightHookId);
+
+    return lst;
+  }
+  mintAmountAndRebalanceAndSendToUser(
+    tx: Transaction,
+    address: string,
+    amount: string,
+  ) {
+    const lst = this.mintAmountAndRebalance(tx, address, amount);
+    tx.transferObjects([lst], address);
+  }
+
+  // returns the sui coin
+  redeem(tx: Transaction, lstId: TransactionObjectInput) {
+    const [sui] = generated.redeem(tx, this.liquidStakingObject.type, {
+      self: this.liquidStakingObject.id,
+      systemState: SUI_SYSTEM_STATE_ID,
+      lst: lstId,
+    });
+
+    return sui;
+  }
+  redeemAmount(tx: Transaction, address: string, amount: string) {
+    const lstCoin = coinWithBalance({
+      balance: BigInt(amount),
+      type: this.liquidStakingObject.type,
+      useGasCoin: false,
+    })(tx);
+
+    const sui = this.redeem(tx, lstCoin);
+
+    return sui;
+  }
+  redeemAmountAndSendToUser(tx: Transaction, address: string, amount: string) {
+    const suiCoin = this.redeemAmount(tx, address, amount);
+    tx.transferObjects([suiCoin], address);
+  }
+
+  // admin functions
+  increaseValidatorStake(
+    tx: Transaction,
+    adminCapId: TransactionObjectInput,
+    validatorAddress: string,
+    suiAmount: number,
+  ) {
+    generated.increaseValidatorStake(tx, this.liquidStakingObject.type, {
+      self: this.liquidStakingObject.id,
+      adminCap: adminCapId,
+      systemState: SUI_SYSTEM_STATE_ID,
+      validatorAddress,
+      suiAmount: BigInt(suiAmount),
+    });
+  }
+
+  decreaseValidatorStake(
+    tx: Transaction,
+    adminCapId: TransactionObjectInput,
+    validatorAddress: string,
+    targetUnstakeSuiAmount: bigint,
+  ) {
+    generated.decreaseValidatorStake(tx, this.liquidStakingObject.type, {
+      self: this.liquidStakingObject.id,
+      adminCap: adminCapId,
+      systemState: SUI_SYSTEM_STATE_ID,
+      validatorAddress,
+      targetUnstakeSuiAmount,
+    });
+  }
+
+  collectFees(tx: Transaction, weightHookAdminCapId: TransactionObjectInput) {
+    const [sui] = weightHookGenerated.collectFees(
+      tx,
+      this.liquidStakingObject.type,
+      {
+        self: this.liquidStakingObject.weightHookId,
+        liquidStakingInfo: this.liquidStakingObject.id,
+        systemState: SUI_SYSTEM_STATE_ID,
+        weightHookAdminCap: weightHookAdminCapId,
+      },
+    );
+
+    return sui;
+  }
+  collectFeesAndSendToUser(
+    tx: Transaction,
+    weightHookAdminCapId: TransactionObjectInput,
+    address: string,
+  ) {
+    const sui = this.collectFees(tx, weightHookAdminCapId);
+    tx.transferObjects([sui], address);
+  }
+
+  updateFees(
+    tx: Transaction,
+    weightHookAdminCapId: TransactionObjectInput,
+    feeConfigArgs: FeeConfigArgs,
+  ) {
+    let [builder] = newBuilder(tx);
+
+    if (feeConfigArgs.mintFeeBps != null) {
+      console.log(`Setting mint fee bps to ${feeConfigArgs.mintFeeBps}`);
+
+      builder = setSuiMintFeeBps(tx, {
+        self: builder,
+        fee: BigInt(feeConfigArgs.mintFeeBps),
+      })[0];
+    }
+
+    if (feeConfigArgs.redeemFeeBps != null) {
+      console.log(`Setting redeem fee bps to ${feeConfigArgs.redeemFeeBps}`);
+      builder = setRedeemFeeBps(tx, {
+        self: builder,
+        fee: BigInt(feeConfigArgs.redeemFeeBps),
+      })[0];
+    }
+
+    if (feeConfigArgs.spreadFeeBps != null) {
+      console.log(`Setting spread fee bps to ${feeConfigArgs.spreadFeeBps}`);
+      builder = setSpreadFeeBps(tx, {
+        self: builder,
+        fee: BigInt(feeConfigArgs.spreadFeeBps),
+      })[0];
+    }
+
+    const [feeConfig] = toFeeConfig(tx, builder);
+
+    weightHookGenerated.updateFees(tx, this.liquidStakingObject.type, {
+      self: this.liquidStakingObject.weightHookId,
+      liquidStakingInfo: this.liquidStakingObject.id,
+      weightHookAdminCap: weightHookAdminCapId,
+      feeConfig,
+    });
+  }
+
+  // weight hook functions
+  initializeWeightHook(tx: Transaction, adminCapId: TransactionObjectInput) {
+    const [weightHook, weightHookAdminCap] = weightHookGenerated.new_(
+      tx,
+      this.liquidStakingObject.type,
+      adminCapId,
+    );
+
+    tx.moveCall({
+      target: `0x2::transfer::public_share_object`,
+      typeArguments: [
+        `${WeightHook.$typeName}<${this.liquidStakingObject.type}>`,
+      ],
+      arguments: [weightHook],
+    });
+
+    return weightHookAdminCap;
+  }
+
+  setValidatorAddressesAndWeights(
+    tx: Transaction,
+    weightHookId: TransactionObjectInput,
+    weightHookAdminCap: TransactionObjectInput,
+    validatorAddressesAndWeights: Record<string, number>,
+  ) {
+    const [vecMap] = tx.moveCall({
+      target: `0x2::vec_map::empty`,
+      typeArguments: ["address", "u64"],
+      arguments: [],
+    });
+
+    for (const [validatorAddress, weight] of Object.entries(
+      validatorAddressesAndWeights,
+    )) {
+      tx.moveCall({
+        target: `0x2::vec_map::insert`,
+        typeArguments: ["address", "u64"],
+        arguments: [
+          vecMap,
+          tx.pure.address(validatorAddress),
+          tx.pure.u64(weight),
+        ],
+      });
+    }
+
+    weightHookGenerated.setValidatorAddressesAndWeights(
+      tx,
+      this.liquidStakingObject.type,
+      {
+        self: weightHookId,
+        weightHookAdminCap,
+        validatorAddressesAndWeights: vecMap,
+      },
+    );
+  }
+
+  rebalance(tx: Transaction, weightHookId: TransactionObjectInput) {
+    weightHookGenerated.rebalance(tx, this.liquidStakingObject.type, {
+      self: weightHookId,
+      systemState: SUI_SYSTEM_STATE_ID,
+      liquidStakingInfo: this.liquidStakingObject.id,
+    });
+  }
+
+  async getSpringSuiApy() {
+    const validatorApys = (await this.client.getValidatorsApy()).apys;
+
+    const liquidStakingInfo = await fetchLiquidStakingInfo(
+      this.liquidStakingObject,
+      this.client,
+    );
+
+    const totalSuiSupply = new BigNumber(
+      liquidStakingInfo.storage.totalSuiSupply.toString(),
+    ).div(10 ** SUI_DECIMALS);
+
+    const spreadFeePercent = new BigNumber(
+      liquidStakingInfo.feeConfig.element?.spreadFeeBps.toString() ?? 0,
+    ).div(100);
+
+    return new BigNumber(
+      liquidStakingInfo.storage.validatorInfos
+        .reduce((acc, validatorInfo) => {
+          const validatorApy = new BigNumber(
+            validatorApys.find(
+              (_apy) => _apy.address === validatorInfo.validatorAddress,
+            )?.apy ?? 0,
+          );
+
+          const validatorTotalSuiAmount = new BigNumber(
+            validatorInfo.totalSuiAmount.toString(),
+          ).div(10 ** SUI_DECIMALS);
+
+          return acc.plus(validatorApy.times(validatorTotalSuiAmount));
+        }, new BigNumber(0))
+        .div(totalSuiSupply),
+    ).times(new BigNumber(1).minus(spreadFeePercent.div(100)));
+  }
+}
+
+// user functions
+export const fetchLiquidStakingInfo = (
+  info: LiquidStakingObjectInfo,
+  client: SuiClient,
+): Promise<LiquidStakingInfo<any>> =>
+  LiquidStakingInfo.fetch(client, phantom(info.type), info.id);
---
+++ sdk/src/lib/transactions.ts
@@ -0,0 +1,38 @@
+import { Transaction } from "@mysten/sui/transactions";
+
+import { LstClient } from "../client";
+
+export const convertLsts = (
+  inLstClient: LstClient,
+  outLstClient: LstClient,
+  transaction: Transaction,
+  address: string,
+  amount: string,
+) => {
+  const suiCoin = inLstClient.redeemAmount(transaction, address, amount);
+  const lstCoin = outLstClient.mint(transaction, suiCoin);
+
+  outLstClient.rebalance(
+    transaction,
+    outLstClient.liquidStakingObject.weightHookId,
+  );
+
+  return lstCoin;
+};
+
+export const convertLstsAndSendToUser = (
+  inLstClient: LstClient,
+  outLstClient: LstClient,
+  transaction: Transaction,
+  address: string,
+  amount: string,
+) => {
+  const lstCoin = convertLsts(
+    inLstClient,
+    outLstClient,
+    transaction,
+    address,
+    amount,
+  );
+  transaction.transferObjects([lstCoin], address);
+};
