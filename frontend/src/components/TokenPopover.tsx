350502f8d5b17f1c9738b8a9b1865ced248e410b
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-01-23 10:03:08 +0800

    remove test

--- frontend/src/components/TokenPopover.tsx
+++ frontend/src/components/TokenPopover.tsx
@@ -48,7 +48,9 @@ export default function TokenPopover({ token, onChange }: TokenPopoverProps) {
         {[
           "SUI",
           ...Object.values(LstId)
-            .filter((lstId) => lstId !== LstId.ripleysSUI)
+            .filter(
+              (lstId) => ![LstId.test1SUI, LstId.ripleysSUI].includes(lstId),
+            )
             .filter(
               (lstId) =>
                 lstId !== LstId.upSUI ||
