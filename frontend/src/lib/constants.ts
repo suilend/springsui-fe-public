e76e740583109b0f2c0a59a66287d9105e353d0b
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-03-06 13:35:10 +0800

    bump

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -4,7 +4,7 @@ export const TITLE = "SpringSui | Infinitely liquid staking on Sui";
 export const DESCRIPTION =
   "Stake SUI with SpringSui and access the power of DeFi with sSUI, our flagship LST. With SpringSui's liquid staking, you can stake SUI and hold sSUI to earn yield while maintaining flexibility. Maximize your DeFi opportunities and enjoy rewards with sSUI, the LST that brings unmatched liquidity to Sui!";
 
-export const SUILEND_ASSETS_URL =
-  "https://suilend-assets.s3.us-east-2.amazonaws.com/suilend";
 export const ASSETS_URL =
   "https://suilend-assets.s3.us-east-2.amazonaws.com/springsui";
+export const SUILEND_ASSETS_URL =
+  "https://suilend-assets.s3.us-east-2.amazonaws.com/suilend";
